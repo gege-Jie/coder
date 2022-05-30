5a389beee65b8af900108218e5bbe3833381884e
wisdon rj <ranjie@wsdinfo.com>
2022-05-30 18:39:52 +0800

    修改Main类。

--- src/com/rj/Main.java
+++ src/com/rj/Main.java
@@ -2,7 +2,9 @@ package com.rj;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+  public static void main(String[] args) {
+    // write your code here
+    System.out.println();
+
+  }
 }
